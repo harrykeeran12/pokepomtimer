88da57b22cd5583db96abf565a8b62f7c278bec6
harrykeeran12 <68599167+harrykeeran12@users.noreply.github.com>
2021-06-07 20:13:08 +0100

    added the icons + started work on the timer

--- react-pokepomtimer/src/components/Timer.js
+++ react-pokepomtimer/src/components/Timer.js
@@ -1,19 +1,30 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
 function Timer(){
-  const [start, setstart] = useState(false)
-  const startTimer = () =>{
+  const [start, setstart] = useState(true)
+  const [Time, setTime] = useState({mins: 10, secs: [0, 0]})
+  
+  const boolTimer = () =>{
     setstart(!start)
+    if(start === true){
+      return null
+    }
+    else{
+      clearInterval(this.interval)
+    }
   }
-    return(
+
+      return(
       <div className="timer-wrapper">
         <section className="timer">
-          <p className="mins">10</p>
+          <p className="mins">{Time.mins}</p>
           <p>:</p>
-          <p className="secs">00</p>
+          <p className="secs">{Time.secs[0]}</p>
+          <p className="secs">{Time.secs[1]}</p>
         </section>
         <div className="icon-circle">
-          <i className={`icon ${start ? 'fa fa-hourglass-start start' : 'fa fa-hourglass-end start' }`} aria-hidden="true" onClick={() => startTimer()}></i>
+          <i className={`icon ${start ? 'fa fa-hourglass-start start' : 'fa fa-hourglass-end start' }`} aria-hidden="true" onClick={() => boolTimer()}></i>
         </div>
       </div>
     )
