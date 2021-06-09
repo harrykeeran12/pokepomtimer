042c74c3fc8bab64873ae8afd9327c02f59f04e0
harrykeeran12 <68599167+harrykeeran12@users.noreply.github.com>
2021-06-09 10:59:51 +0100

    switched from react-tooltip to tippy.js
    took out some code i didn't need

--- react-pokepomtimer/src/components/Timer.js
+++ react-pokepomtimer/src/components/Timer.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import ReactTooltip from "react-tooltip";
+import Tippy from '@tippyjs/react';
+
+import 'tippy.js/dist/tippy.css';
+
 
 function Timer() {
   const [start, setstart] = useState(false);
@@ -20,11 +23,11 @@ function Timer() {
             setseconds(59);
             setminutes(minutes - 1);
           } else {
-            if (skip === false) {
+            /* if (skip === false) {
               setminutes(5);
             } else {
               setminutes(10);
-            }
+            } */
           }
         } else {
           setseconds(seconds - 1);
@@ -53,45 +56,60 @@ function Timer() {
 
   return (
     <div className="timer-wrapper">
-      <div>
-        <i class="fa fa-circle-o" aria-hidden="true"></i>
-      </div>
       <section className="timer">
         <p className="mins">{timerMins}</p>
         <p>:</p>
         <p className="secs">{timerSecs}</p>
       </section>
       <div className="icon-grouping">
-        <i
-          className={`icon ${
-            start ? "fa fa-hourglass-start start" : "fa fa-hourglass-end start"
-          }`}
-          aria-hidden="true"
-          onClick={() => boolTimer()}
-          data-tip
-          data-for="hourglass"
-        ></i>
-        <i
-          class="fa fa-step-forward icon"
-          aria-hidden="true"
-          onClick={() => skipBreak()}
-          data-tip
-          data-for="skip-icon"
-        ></i>
+        <Tippy
+          content={<p>Click to {start ? "pause" : "play"} the timer</p>}
+          className="tooltip"
+          delay="10"
+          placement="top"
+          
+        >
+          <i
+            className={`icon ${
+              start
+                ? "fa fa-hourglass-start start"
+                : "fa fa-hourglass-end start"
+            }`}
+            aria-hidden="true"
+            onClick={() => boolTimer()}
+            data-tip
+            data-for="hourglass"
+            tabindex="0"
+          ></i>
+        </Tippy>
+        <Tippy
+          content={
+            skip ? (
+              <p>Click to unskip the break.</p>
+            ) : (
+              <p>Click to skip the break.</p>
+            )
+          }
+          className="tooltip"
+        >
+          <i
+            class="fa fa-step-forward icon"
+            aria-hidden="true"
+            onClick={() => skipBreak()}
+            data-tip
+            data-for="skip-icon"
+            tabindex="0"
+          ></i>
+        </Tippy>
       </div>
 
-      <ReactTooltip id="hourglass tooltip" type="info" effect="solid">
-        <p>Click to {start ? "pause" : "play"} the timer</p>
+      {/* <ReactTooltip id="hourglass tooltip" type="info" effect="solid">
+        
       </ReactTooltip>
-      {skip ? (
-        <ReactTooltip id="skip-icon tooltip" type="info" effect="solid">
-          <p>Click to unskip the break.</p>
-        </ReactTooltip>
-      ) : (
-        <ReactTooltip id="skip-icon tooltip" type="info" effect="solid">
-          <p>Click to skip the break.</p>
-        </ReactTooltip>
-      )}
+      <ReactTooltip id="skip-icon tooltip" type="info" effect="solid">
+          
+        </ReactTooltip> 
+        currently does not work*/}
     </div>
   );
 }
